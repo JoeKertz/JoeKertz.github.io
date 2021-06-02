06bc901c53843c4b7c954e565bf97382d2ee4e62
JoeKertz <jsk13b@acu.edu>
2021-06-01 19:50:40 -0500

    RestructureWebpage

---
+++ js/CV.js
@@ -0,0 +1,40 @@
+$( ".experiences" ).click(function() {
+    $( ".experience" ).toggle( "fast" );
+  });
+  $( ".educations" ).click(function() {
+      $( ".education" ).toggle( "fast" );
+  });
+  
+  
+  //START BUTTON TWIST
+  $(".educations").on('click',function(){
+      $("#educationButton").toggleClass("active");
+  });
+  
+  $(".experiences").on('click',function(){
+      $("#experienceButton").toggleClass("active");
+  });
+  
+  $(".skills").on('click',function(){
+      $("#skillButton").toggleClass("active");
+  });
+  
+  $(".certifications").on('click',function(){
+      $("#certificationsButton").toggleClass("active");
+  });
+  
+  $(".software").on('click',function(){
+      $("#projectsButton").toggleClass("active");
+  });
+  //END BUTTON TWIST
+  
+  
+  $( ".skills" ).click(function() {
+      $( ".skill" ).toggle( "fast" );
+  });
+  $( ".software" ).click(function() {
+      $( ".column" ).toggle( "fast" );
+  });
+  $( ".certifications" ).click(function() {
+      $( ".certification" ).toggle( "fast" );
+  });
